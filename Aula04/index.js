3f8a3f70af7baeb87ea42cdb11dfd22a313f4396
Lenix Eduardo <lenix.camargo@gmail.com>
2021-06-07 21:29:35 -0300

    Aula04

---
+++ Aula04/index.js
@@ -0,0 +1,53 @@
+// //let Pessoa = {
+//     nome : 'Lenix Eduardo',
+
+//     idade : 23,
+    
+//     eMaiorDeIdade : true
+// }
+
+// console.log(Pessoa.nome)
+
+
+
+let animais = ["gato,coelho,cachorro,esquilo"];
+
+console.log(animais)
+
+
+let numero1 = 25
+
+let numero2 = 30
+
+let resultadoSoma = numero1 + numero2
+
+console.log('O resultado da soma entre 30 + 25 é =' + resultadoSoma)
+
+let resultadoSubtracao = numero1 - numero2
+
+let resultadoDivisao = numero1 / numero2
+
+
+let resultadoMultiplicacao = numero1 * numero2
+
+
+console.log('O resultado da subtracao entre 30 - 25 é = ' + resultadoSubtracao)
+
+console.log('O resultado da divisao  entre 30 / 25 é = ' + resultadoDivisao)
+
+console.log('O resultado da multriplicacao entre 30 * 25 é = ' +resultadoMultiplicacao)
+
+let x = 30
+
+let y = 30
+
+if(x > y){
+    console.log('O número ' + x + ' é maior que ' + y)
+    
+}else if( x < y){
+    console.log('O número ' +  y + '  é maior que ' + x)}
+else( x = y)
+    console.log('O número ' + x + ' é igual a ' + y)
+
+
+
